0217d80caa618db41ce8364981c2ea3e4f2b9dc6
GonzaloVeron <gonveron96@gmail.com>
2019-12-31 02:14:14 -0300

    Agregado servidor mongo y creado GenericMongoDAO y CharacterService

---
+++ Argentum/Assets/MongoConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MongoDB.Driver;
+
+public class MongoConnection
+{
+    private static MongoConnection INSTANCE;
+
+    private MongoClient client;
+
+    private MongoDatabase db;
+
+    public static MongoConnection getInstance()
+    {
+        if(INSTANCE == null)
+        {
+            INSTANCE = new MongoConnection();
+        }
+        return (INSTANCE);
+    }
+
+    public MongoConnection()
+    {
+        this.client = new MongoClient("mongodb://localhost:27017/AO");
+        this.db = this.client.GetServer().GetDatabase("AO");
+    }
+
+    public MongoDatabase getDB()
+    {
+        return this.db;
+    }
+}
---
+++ Argentum/Assets/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State
+{
+    public int lifePoints;
+    public int manaPoints;
+    public int hungryPoints;
+    public int thirstPoints;
+
+    public State(int lifePoints, int manaPoints, int hungryPoints, int thirstPoints)
+    {
+        this.lifePoints = lifePoints;
+        this.manaPoints = manaPoints;
+        this.hungryPoints = hungryPoints;
+        this.thirstPoints = thirstPoints;
+    }
+
+}
