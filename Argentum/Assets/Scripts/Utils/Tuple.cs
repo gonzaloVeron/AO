62f9dfcf1c5160373b4d956afff94ccfae74e7ff
GonzaloVeron <gonveron96@gmail.com>
2020-01-13 18:06:05 -0300

    override a getHash() de tupla

--- Argentum/Assets/Scripts/Utils/Tuple.cs
+++ Argentum/Assets/Scripts/Utils/Tuple.cs
@@ -13,11 +13,15 @@ public class Tuple<T, Y>
         this.item1 = t;
         this.item2 = y;
     }
-
     public override bool Equals(object obj)
     {
         Tuple<T, Y> tu = (Tuple<T, Y>)obj;
         return this.item1.Equals(tu.item1) && this.item2.Equals(tu.item2);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
     }
 
 }
