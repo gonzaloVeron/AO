2d19092ff527cc9741eaaeb4a28fbe070e752e48
GonzaloVeron <gonveron96@gmail.com>
2020-08-11 02:36:30 -0300

    saco mono a ver que tul

--- Argentum/build.sh
+++ Argentum/build.sh
@@ -11,15 +11,16 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 	-batchmode \
 	-nographics \
 	-silent-crashes \
-	-logFile $(pwd)/unity.log \
 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
 	-runTests -testPlatform editmode \
+	-logFile \
 	-testResults $(pwd)/test.xml \
 	-quit
 
 rc0=$?
 echo "Unit test logs"
 cat $(pwd)/test.xml
+echo rc0
 # exit if tests failed
 if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
 
