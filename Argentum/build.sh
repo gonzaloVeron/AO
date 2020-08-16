9295f2fd1c3ced80f736906fd71b2f7f53beb37a
GonzaloVeron <gonveron96@gmail.com>
2020-08-16 19:18:17 -0300

    travis y la re putisima madre loco

--- Argentum/build.sh
+++ Argentum/build.sh
@@ -37,26 +37,16 @@ mkdir ~/Library/Unity/Certificates
 cp CACerts.pem ~/Library/Unity/Certificates/
 
 echo "activate license"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -serial I3-GKE5-PKF4-XXXX-XXXX-XXXX -username "gonveron96@gmail.com" -password "Tiranosaurio0" -logfile 
+
+
+echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-quit \
-	-batchmode \
-	-serial I3-GKE5-PKF4-XXXX-XXXX-XXXX \
-	-username "gonveron96@gmail.com" \
-	-password "Tiranosaurio0" \
+	-nographics \
 	-projectPath "$(pwd)" \
 	-runTests \
 	-testResults "$(pwd)/testLogs.xml"\
-	-logfile "$(pwd)/logs.txt"
-
-
-#echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
-#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#	-batchmode \
-#	-nographics \
-#	-projectPath "$(pwd)" \
-#	-runTests \
-#	-testResults "$(pwd)/testLogs.xml"\
-#	-logFile "$(pwd)/logs.txt"
+	-logFile "$(pwd)/logs.txt"
 
 echo "┌───────────────────┐"
 echo "│  Unity test logs  │"
