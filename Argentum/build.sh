6fe5d6bad895412a5fa43f30840b7e5b1422803f
GonzaloVeron <gonveron96@gmail.com>
2020-08-16 19:25:29 -0300

    a ver

--- Argentum/build.sh
+++ Argentum/build.sh
@@ -37,7 +37,7 @@ mkdir ~/Library/Unity/Certificates
 cp CACerts.pem ~/Library/Unity/Certificates/
 
 echo "activate license"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -serial I3-GKE5-PKF4-XXXX-XXXX-XXXX -username "gonveron96@gmail.com" -password "Tiranosaurio0" -logfile 
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -serial I3-GKE5-PKF4-XXXX-XXXX-XXXX -username "gonveron96@gmail.com" -password "Tiranosaurio0" -logfile "$(pwd)/logs.txt"
 
 
 echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
@@ -46,7 +46,7 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 	-projectPath "$(pwd)" \
 	-runTests \
 	-testResults "$(pwd)/testLogs.xml"\
-	-logFile "$(pwd)/logs.txt"
+#	-logFile "$(pwd)/logs.txt"
 
 echo "┌───────────────────┐"
 echo "│  Unity test logs  │"
